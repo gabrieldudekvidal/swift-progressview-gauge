51ab88fd5972420ae04deaa7f2a539300ec36daf
Gabriel Vidal <gabrieldkvl@icloud.com>
2026-07-04 13:50:50 +0800

    Initial Commit

---
+++ ProgressViewAndGauge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProgressViewAndGauge
+//
+//  Created by Gabriel Vidal on 2026-07-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProgressViewAndGauge/ProgressViewAndGaugeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewAndGaugeApp.swift
+//  ProgressViewAndGauge
+//
+//  Created by Gabriel Vidal on 2026-07-04.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewAndGaugeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
