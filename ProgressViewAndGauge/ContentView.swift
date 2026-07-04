0e2f2192ec614648e68c2db2a3ce8e9dd8345f09
Gabriel Vidal <gabrieldkvl@icloud.com>
2026-07-04 14:09:49 +0800

    ProgressView and Gauge

--- ProgressViewAndGauge/ContentView.swift
+++ ProgressViewAndGauge/ContentView.swift
@@ -1,19 +1,44 @@
-//
-//  ContentView.swift
-//  ProgressViewAndGauge
-//
-//  Created by Gabriel Vidal on 2026-07-04.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var progress: Double = 0.0
+    @State private var batteryLevel: Double = 0.80
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // Loading
+            ProgressView("Charging")
+            
+            // Progress bar
+            ProgressView(value: progress, total: 1.0)
+            Button("Start") {
+                withAnimation {
+                    progress += 0.1
+                }
+            }
+            
+            // Gauge basic
+            Gauge(value: 0.65) {
+                Text("Battery")
+            } currentValueLabel: {
+                Text("65%")
+            }
+            .tint(.yellow)
+            
+            // Gauge
+            Gauge(value: batteryLevel, in: 0 ... 1) {
+                Text("Battery Level")
+            } currentValueLabel: {
+                Text("\(Int(batteryLevel * 100)) %")
+            } minimumValueLabel: {
+                Text("0%")
+            } maximumValueLabel: {
+                Text("100%")
+            }
+            .gaugeStyle(.accessoryCircularCapacity)
+            .tint(.green)
+            .scaleEffect(1.5)
+            .padding()
         }
         .padding()
     }
